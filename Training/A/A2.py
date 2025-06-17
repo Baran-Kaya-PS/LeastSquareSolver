d7df39fc0e8563d9503f2bb3fc50addf266ea3b2
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-06-17 17:48:10 +0200

    exercice 2 using useful functions of numpy

---
+++ Training/A/A2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+shape_arg = (1,2)
+
+print(np.zeros((8,8)))
+
+print()
+
+
+print(np.ones((5,3)))
+
+print()
+
+
+x = np.full((1,6),'k')
+
+print(x)
+
+print(x.itemsize) # nb byte of each elements
