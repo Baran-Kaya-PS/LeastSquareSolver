44a753f0e2facb8698c6b30f7528b923131f710e
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-06-25 18:41:12 +0200

    difference in time with numpy adding and my own adding function...

---
+++ Training/A/A8.py
@@ -0,0 +1,25 @@
+import time
+import numpy as np
+
+M1 = np.random.rand(500,500)
+
+M2 = np.random.rand(500,500)
+
+start = time.time()
+M3 = M1 + M2
+end = time.time()
+
+print(f"Elapsed : {end - start} seconds")
+def matadd_(A,B):
+    if np.shape(A) == np.shape(B):
+        M3 = np.zeros_like(A)
+        for i in range(500):
+            for j in range(500):
+                M3[i,j] = A[i,j] + B[i,j]
+        return M3
+    return None
+
+start = time.time()
+M3 = matadd_(M1,M2)
+end = time.time()
+print(f"Elapsed : {end - start} seconds")
