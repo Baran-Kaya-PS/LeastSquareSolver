bc6a3ddb4c4424e46ddf0c98b1fd681f82c77f67
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-07-06 20:03:47 +0200

    generate random matrices function, and insert nan into the matrix randomly

--- Training/B/B15_NaN.py
+++ Training/B/B15_NaN.py
@@ -2,11 +2,22 @@ import numpy as np
 from keras.utils.layer_utils import print_summary
 
 
-def gen_random_mat(n_rows,n_cols,low=0.0,high=1,seed=None) -> np.ndarray:
-    pass
+def gen_random_mat(n_rows,n_cols,seed=None) -> np.ndarray:
+    if n_cols <= 0 or n_rows <= 0:
+        raise ValueError("nrows ou ncols doivent être positifs")
+    rng = np.random.default_rng(seed)
+    return rng.random((n_rows,n_cols))
 
-def insert_nans(mat,nan_ratio=0.1,seed=None) -> np.ndarray:
-    pass
+def insert_nans(mat,nan_ratio=0.1,seed=None) :#-> np.ndarray:
+    if not (0 <= nan_ratio <= 1):
+        raise ValueError("nan_ration doit être entre 0 et 1")
+    n_rows, n_cols = mat.shape
+    rng = np.random.default_rng(seed)
+    for i in range(n_rows):
+        for j in range(n_cols):
+            if rng.random() < nan_ratio:
+                mat[i,j] = np.nan
+    return mat
 
 def count_nans(mat) -> int:
     pass
@@ -20,5 +31,8 @@ def col_means(mat) -> np.ndarray[int]:
 def fill_nans_with_col_mean(mat,inplace=False)-> np.ndarray:
     pass
 
-def print_summary(mat):
-    pass
+M = gen_random_mat(3,3)
+
+print(M)
+
+print(insert_nans(M))
